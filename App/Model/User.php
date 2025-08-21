297e9372656a1f9996d636631d98f0aebf4e5b10
mithridatem <mathieumithridate@adrar-formation.com>
2025-08-21 13:50:07 +0200

    feat : model

---
+++ App/Model/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Model;
+
+class User {
+    //Attributs
+    private int $idUser;
+    private string $firstname;
+    private string $lastname;
+    private string $email;
+    private string $password;
+    private ?string $img;
+
+    //Getters et Setters
+    public function getIdUser(): int
+    {
+        return $this->idUser;
+    }
+
+    public function setIdUser(int $id): void
+    {
+        $this->idUser = $id;
+    }
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(?string $img): void
+    {
+        $this->img = $img;
+    }
+}
